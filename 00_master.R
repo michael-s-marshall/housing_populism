886f2c7adfc098b806755a7a0f5774a0959e66f9
michael-s-marshall <michael.marshall@sheffield.ac.uk>
2026-03-02 09:27:41 +0000

    Updating master post-cleaning up of repo.

--- 00_master.R
+++ 00_master.R
@@ -1,19 +1,15 @@
 rm(list = ls())
 
 # running scripts
-system("Rscript 00_creating_level_twos_2021.R")
-system("Rscript 01_income_predictor_2021.R")
-system("Rscript 02_uni_predictor_2021.R")
-system("Rscript 03_creating_2021_dataset.R")
-system("Rscript 04_modelling_2021.R")
+system("Rscript 01_creating_level_twos_2021.R")
+system("Rscript 02_creating_2021_dataset.R")
+system("Rscript 03_multiple_imputation_2021.R")
+system("Rscript 04_mi_prices_2021.R")
 
 system("Rscript 05_creating_level_twos_2024.R")
-system("Rscript 06_income_predictor_2024.R")
-system("Rscript 07_uni_predictor_2024.R")
-system("Rscript 08_creating_2024_dataset.R")
-system("Rscript 09_modelling_2024.R")
+system("Rscript 06_creating_2024_dataset.R")
+system("Rscript 07_multiple_imputation_2024.R")
+system("Rscript 08_mi_prices_2024.R")
 
-system("Rscript 10_maps.R")
-
-system("Rscript 12_between_census_comparisons.R")
-system("Rscript 13_multiple_imputation.R")
+system("Rscript 09_maps.R")
+system("Rscript 10_between_census_comparisons.R")
