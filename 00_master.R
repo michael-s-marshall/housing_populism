bbafe64661481cdddd912fd3725e2642f3b3e429
michael-s-marshall <michael.marshall@sheffield.ac.uk>
2026-03-03 10:37:04 +0000

    Updating master to reflect new script names.

--- 00_master.R
+++ 00_master.R
@@ -3,12 +3,12 @@ rm(list = ls())
 # running scripts
 system("Rscript 01_creating_level_twos_2021.R")
 system("Rscript 02_creating_2021_dataset.R")
-system("Rscript 03_multiple_imputation_2021.R")
+system("Rscript 03_mi_modelling_2021.R")
 system("Rscript 04_mi_prices_2021.R")
 
 system("Rscript 05_creating_level_twos_2024.R")
 system("Rscript 06_creating_2024_dataset.R")
-system("Rscript 07_multiple_imputation_2024.R")
+system("Rscript 07_mi_modelling_2024.R")
 system("Rscript 08_mi_prices_2024.R")
 
 system("Rscript 09_maps.R")
