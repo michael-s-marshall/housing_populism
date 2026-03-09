1ab23602a0ec160d13e30ad19326025e93c30b9f
michael-s-marshall <mmarshall7@sheffield.ac.uk>
2026-03-09 19:31:53 +0000

    Loading mitml package in 2024 modelling script. Logreg multiple imputation in prices modelling 2024.

--- 08_mi_prices_2024.R
+++ 08_mi_prices_2024.R
@@ -54,6 +54,7 @@ meth <- init$method
 pred <- init$predictorMatrix
 
 meth["income"] <- "2l.pan"
+meth["uni"] <- "logreg"
 meth["social_housing.prices"]   <- "~ I(social_housing * prices)"
 meth["homeowner.prices"]   <- "~ I(homeowner * prices)"
 
@@ -61,7 +62,12 @@ pred[,"LAD"] <- -2
 pred["LAD","LAD"] <- 0
 pred["income", "brexit_party"] <- 1
 pred["income", "income"] <- 0
-pred
+pred["uni","LAD"] <- 0
+pred["uni","disabled"] <- 0
+pred["uni","part_time"] <- 0
+pred["uni","full_time"] <- 0
+pred["uni",]
+pred["income",]
 
 # multiple imputation
 imp_mice <- mice(dat, method = meth, predictorMatrix = pred, m = 5, maxit = 5, seed = 123, printFlag = FALSE)
